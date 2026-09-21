23dbc1020de174be5117a0e99261bf6c266843be
mustafajoudeh <mustafa.joudeh2@gmail.com>
2026-09-21 14:58:36 +0300

    Js Taskes

---
+++ Mon-21-9-2026/prompet_alert.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// alert("hello user");
+let user_input = prompt("enter your age =");
+
+console.log(user_input);
+console.log(typeof user_input);
+
+let user_input_to_Number = Number(user_input);
+console.log(typeof user_input_to_Number);
---
+++ Mon-21-9-2026/script.js
@@ -0,0 +1,94 @@
+// // // var x = 10;
+
+// // // console.log(x);
+
+// // // var x = 20;
+
+// // // console.log(x);
+
+// // function test_function() {
+// //   var z = 3;
+// //   console.log(z);
+// // }
+
+// // test_function();
+
+// // "use strict";
+
+// // let y = 10;
+// // z = 5;
+// // console.log(`y = ${y}`);
+// // console.log(`z = ${z}`);
+
+// let name = "Mustafa";
+// let number = 10;
+// let double = 2.5;
+// let boolean = true;
+// let null_value = null;
+// let und_val = undefined;
+// let obj_val = { name: "Mustafa", age: 22 };
+
+// console.log(`data type of name ${typeof name}`);
+// console.log(`data type of number ${typeof number}`);
+// console.log(`data type of double ${typeof double}`);
+// console.log(`data type of boolean ${typeof boolean}`);
+
+// console.log(`data type of null_value ${typeof null_value}`);
+// console.log(`data type of und_val ${typeof und_val}`);
+// console.log(`data type of obj_val ${typeof obj_val}`);
+
+// ========================================================================
+// ===========================Operation====================================
+// ========================================================================
+
+// // + , - , * , /
+
+// let a = 10;
+// let b = 20;
+
+// let sum = a + b;
+
+// console.log(`a + b = ${sum}`);
+
+// // Logical operation and , or , not
+
+// let c = 2;
+// let d = 10;
+
+// let and_op = c && d;
+
+// console.log(and_op);
+
+// //Condition statemnts
+
+// if (d > c) {
+//   console.log("d geater than c");
+// } else {
+//   console.log("c geater than d");
+// }
+
+// let day = "sunday";
+
+// switch (day) {
+//   case "sunday":
+//     console.log("this our day");
+//     break;
+//   case "monday":
+//     console.log("this not our day");
+//     break;
+//   case "tusday":
+//     console.log("this not our day");
+//     break;
+
+//   default:
+//     console.log("not found");
+//     break;
+// }
+
+//string method
+
+let string_val = "orange academy";
+
+console.log(string_val.toUpperCase());
+let string_lo_val = "ORANGE ACADEMY";
+console.log(string_lo_val.toLowerCase());
